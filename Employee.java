92ebc8bfe8a365eb808776d7c71fee3acfb9324e
Md Azhar <mdazharstar961@gmail.com>
2025-01-07 18:32:59 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,46 @@
+package com.database.employee_hibernate;
+
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //Specifying Inheritance property and Strategy using single table only as requirement
+/*----------Specifying First Table Type may be 'int , char or String'---------------*/
+@DiscriminatorColumn(name= "Employee_Type",discriminatorType = DiscriminatorType.STRING) //No use in remaining child classes
+@DiscriminatorValue(value = "Normal_Employee") //Specifying First Table name
+public class Employee 
+{
+	/*Here we creating 3 Different table includes Base class and 2 Derived classes*/
+	
+	@Id
+	private String empId;
+	private String empname;
+	/*-----------------------Parameterised constructor---------------------------*/
+	public Employee(String empId, String empname) 
+	{
+		super();
+		this.empId = empId;
+		this.empname = empname;
+	}
+	/*--------------------------------------------------------------------------*/
+	public Employee() 
+	{
+		super(); // TODO Auto-generated constructor stub	
+	}
+	/*------------------------Getter and Setters----------------------------*/
+	public String getEmpId() {return empId;}
+
+	public void setEmpId(String empId) {this.empId = empId;}
+
+	public String getEmpname() {return empname;}
+
+	public void setEmpname(String empname) {this.empname = empname;}
+	/*--------------------------------------------------------------------------*/
+	@Override
+	public String toString() 
+	{
+		return "\n ------Employee-------"
+				+ "\n Employee Id: " + empId + 
+				"\n Employee name: " + empname 
+				+"-------------------------------";
+	}	
+}
